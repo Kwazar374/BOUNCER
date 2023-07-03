2b1747a25f6fde522038572b8b42c69cfbb3436d
Kwazar374 <66804584+Kwazar374@users.noreply.github.com>
2023-07-03 18:35:27 +0200

    Ball Mechanics
    working ball mechanics (collisions with player-paddle and collisions with walls)

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 import pygame
+import random
 
 # pygame setup
 pygame.init()
 width, height = screen_size = (800, 500)
 screen = pygame.display.set_mode(screen_size)
 pygame.display.set_caption('PONG BY KWAZAR374')
-icon = pygame.image.load('assets/arts/iconB.png')
+icon = pygame.image.load('assets/arts/iconB.png').convert()
 pygame.display.set_icon(icon)
 clock = pygame.time.Clock()
 running = True
 
 # background setup
-background = pygame.image.load('assets/arts/background_1.png')
+background = pygame.image.load('assets/arts/background_1.png').convert()
 
 # paddles and ball setup
 player_surf = pygame.image.load('assets/arts/paddle.png').convert()
@@ -24,8 +25,19 @@ ball_surf = pygame.image.load('assets/arts/ball.png').convert()
 ball_rect = ball_surf.get_rect(center=(200, 200))
 
 # game variables setup:
-paddle_acceleration = 3
+# paddle
+paddle_acceleration = 2
 paddle_speed = 0
+paddle_speed_limit = 10
+
+# ball
+round_active = False
+ball_direction = -1 # -1 if ball goes left and 1 if ball goes right
+ball_angle = 0 # =========================================
+# 1 if ball flies down at a 45-degree angle to the paddle
+# 0 if ball flies perpendicular to the paddle
+# -1 if ball flies up at a 45-degree angle to the paddle
+ball_speed = 8
 
 # game loop
 while running:
@@ -38,12 +50,28 @@ while running:
         if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
             running = False
         
+    # ball (start procedure)
+    if not round_active:
+        ball_start_y_pos = random.choice((random.randint(20, 90), random.randint(height-90, height-20)))
+        ball_rect.center = (width/2, ball_start_y_pos)
+        round_active = True
+        ball_direction = -1
+        if ball_start_y_pos < height/2:
+            ball_angle = 1
+        else:
+            ball_angle = -1
+    
+    # ball (movement)
+    ball_rect.x += ball_speed * ball_direction
+    ball_rect.y += ball_speed * ball_angle
+    if ball_rect.x < 0 or ball_rect.x > width:
+        round_active = False
 
     # player movement
     keys_pressed = pygame.key.get_pressed()
     if not (keys_pressed[pygame.K_w] or keys_pressed[pygame.K_s]):
         paddle_speed = 0
-    elif paddle_speed < 15:
+    elif paddle_speed < paddle_speed_limit:
         paddle_speed += paddle_acceleration
     if keys_pressed[pygame.K_w]:
         player_rect.top -= paddle_speed
@@ -52,8 +80,20 @@ while running:
         player_rect.bottom += paddle_speed
         if player_rect.bottom > height: player_rect.bottom = height-5
 
-    print(player_rect.top, player_rect.bottom)
-    
+    # collisions
+    # ball with player
+    if player_rect.colliderect(ball_rect) == True:
+        collision_point = ball_rect.midleft
+        ball_direction = 1
+        if collision_point[1] - player_rect.topright[1] < 20:
+            ball_angle = -1
+        elif collision_point[1] - player_rect.topright[1] < 34:
+            ball_angle = 0
+        else:
+            ball_angle = 1
+    # ball with walls
+    if ball_rect.midbottom[1] <= 0 or ball_rect.midbottom[1] >= height:
+        ball_angle *= -1
 
     # update the screen
     screen.blit(background, (0, 0))

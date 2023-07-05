bef7cc2669d46f280b89d409ba4fe0b61471a1a3
Kwazar374 <66804584+Kwazar374@users.noreply.github.com>
2023-07-05 19:28:57 +0200

    60-degree bounce instead of 40-degree one
    Glitches of the ball in 60-degree bounce mode has been fixed.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 import random
+import math
 
 # pygame setup
 pygame.init()
@@ -25,6 +26,10 @@ ball_surf = pygame.image.load('assets/arts/ball.png').convert()
 ball_rect = ball_surf.get_rect(center=(200, 200))
 
 # game variables setup:
+
+# gamemode
+retro_mode_on = False
+
 # paddle
 paddle_acceleration = 2
 paddle_speed = paddle_start_speed = 2
@@ -42,21 +47,45 @@ round_active = False
 ball_direction = -1 # -1 if ball goes left and 1 if ball goes right
 ball_angle = 0 # =========================================
 # 1 if ball flies down at a 45-degree angle to the paddle
+# (non-retro mode) 0.5 if ball flies down at a 60-degree angle to the paddle
 # 0 if ball flies perpendicular to the paddle
+# (non-retro mode) -0.5 if ball flies up at a 60-degree angle to the paddle
 # -1 if ball flies up at a 45-degree angle to the paddle
 ball_speed = 9
 ball_simulated = False
+last_wall_collision = -1
 
 # methods
     
 def simulate_ball_trajectory(ball_rects, ball_dir, ball_ang, ball_sped):
-    while ball_rects.right < enemy_rect.left: 
-        ball_rects.x += ball_sped * ball_dir
-        ball_rects.y += ball_sped * ball_ang
-        if ball_rects.midtop[1] <= 0 or ball_rects.midbottom[1] >= height:
-            ball_ang *= -1
-    return ball_rects.centery
-        
+    if retro_mode_on:
+        while ball_rects.right < enemy_rect.left: 
+            ball_rects.x += ball_sped * ball_dir
+            ball_rects.y += ball_sped * ball_ang
+            if ball_rects.midtop[1] <= 0 or ball_rects.midbottom[1] >= height:
+                ball_ang *= -1
+        return ball_rects.centery
+    else:
+        last_wall_collision_counter = 20
+        while ball_rects.right < enemy_rect.left:
+            last_wall_collision_counter += 1
+            if ball_ang in (-1, 1):
+                ball_rects.x += (ball_sped * math.sqrt(2) / 2) * ball_dir
+                ball_rects.y += (ball_sped * math.sqrt(2) / 2) * ball_ang
+            elif ball_ang == 0:
+                ball_rects.x += ball_sped * ball_dir
+            elif ball_ang in (-0.5, 0.5):
+                ball_rects.x += (ball_sped * math.sqrt(3) / 2) * ball_dir
+                ball_rects.y += (ball_sped / 2) * int(ball_ang/0.5)
+            if ball_rects.midtop[1] <= 0 or ball_rects.midbottom[1] >= height:
+                if retro_mode_on:
+                    ball_ang *= -1
+                elif last_wall_collision_counter > 20:
+                    # in some specific cases wall-ball collision procedure was called several times 
+                    # in short time intervals and it was resulting in ball glitches
+                    ball_ang *= -1
+                    last_wall_collision_counter = 0
+        return ball_rects.centery
 
 # game loop
 while running:
@@ -80,10 +109,24 @@ while running:
         ball_angle = random.choice((-1, 1))
     
     # ball (movement)
-    ball_rect.x += ball_speed * ball_direction
-    ball_rect.y += ball_speed * ball_angle
-    if ball_rect.x < -100 or ball_rect.x > width+100:
-        round_active = False
+    if retro_mode_on:
+        ball_rect.x += ball_speed * ball_direction
+        ball_rect.y += ball_speed * ball_angle
+        if ball_rect.x < -100 or ball_rect.x > width+100:
+            round_active = False
+    else:
+        if ball_angle in (-1, 1):
+            ball_rect.x += (ball_speed * math.sqrt(2) / 2) * ball_direction
+            ball_rect.y += (ball_speed * math.sqrt(2) / 2) * ball_angle
+        elif ball_angle == 0:
+            ball_rect.x += ball_speed * ball_direction
+        elif ball_angle in (-0.5, 0.5):
+            ball_rect.x += (ball_speed * math.sqrt(3) / 2) * ball_direction
+            ball_rect.y += (ball_speed / 2) * int(ball_angle/0.5)
+        if ball_rect.x < -100 or ball_rect.x > width+100:
+            round_active = False
+
+
 
     # player movement
     keys_pressed = pygame.key.get_pressed()
@@ -104,6 +147,13 @@ while running:
             if not ball_simulated:
                 bounce_point  = simulate_ball_trajectory(ball_rect.copy(), ball_direction, ball_angle, ball_speed)
                 enemy_dest = bounce_point + random.choice((-20, 0, 20))
+                print(enemy_dest, enemy_rect.height//2)
+                if enemy_dest + enemy_rect.height//2 > height - 5:
+                    enemy_dest -= enemy_dest + enemy_rect.height//2 - height + 5
+                    print('1', enemy_dest)
+                elif enemy_dest - enemy_rect.height//2 < 5:
+                    enemy_dest += enemy_rect.height//2 - enemy_dest + 5
+                    print('2', enemy_dest)
                 ball_simulated = True
 
             if enemy_dest == enemy_rect.centery:
@@ -113,7 +163,7 @@ while running:
                     enemy_speed += enemy_acceleration
                 if abs(enemy_dest - enemy_rect.centery) < enemy_speed:
                     enemy_speed = enemy_start_speed
-                elif enemy_rect.bottom < height and enemy_rect.top > 0:
+                else:
                     if enemy_dest < enemy_rect.centery:
                         enemy_rect.centery -= enemy_speed
                     elif enemy_dest > enemy_rect.centery:
@@ -130,26 +180,34 @@ while running:
         collision_point = ball_rect.midleft
         ball_direction = 1
         if collision_point[1] - player_rect.topright[1] < 30:
-            ball_angle = -1
+            ball_angle = -0.5
         elif collision_point[1] - player_rect.topright[1] < 50:
             ball_angle = 0
         else:
-            ball_angle = 1
+            ball_angle = 0.5
     # ball with enemy
     if enemy_rect.colliderect(ball_rect) == True:
         collision_point = ball_rect.midright
         ball_direction = -1
         if collision_point[1] - enemy_rect.topleft[1] < 23:
-            ball_angle = -1
+            ball_angle = -0.5
         elif collision_point[1] - enemy_rect.topleft[1] < 47:
             ball_angle = 0
         else:
-            ball_angle = 1
+            ball_angle = 0.5
         ball_simulated = False
         reaction_counter = 0
     # ball with walls
     if ball_rect.midtop[1] <= 0 or ball_rect.midbottom[1] >= height:
-        ball_angle *= -1
+        if retro_mode_on:
+            ball_angle *= -1
+        else:
+            # in some specific cases wall-ball collision procedure was called several times 
+            # in short time intervals and it was resulting in ball glitches
+            if last_wall_collision == -1 or pygame.time.get_ticks() - last_wall_collision > 20:
+                ball_angle *= -1
+                last_wall_collision = pygame.time.get_ticks()
+
 
     # update the screen
     screen.blit(background, (0, 0))
